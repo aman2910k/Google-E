c6ccc85bfa7b0abc15c01c4f764197fd091e48c5
AMAN <AMAN@aman>
2017-10-26 21:18:01 -0500

    Commit Google-E

---
+++ src/google/Google.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package google;
+
+import google.newpackage.gooooog;
+
+/**
+ *
+ * @author aa
+ */
+public class Google {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         gooooog ad=new gooooog();
+        ad.show();
+    }
+}
